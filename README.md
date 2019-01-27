38322e746b4ede36a6f4eed31bfe23773653534b
kevin <kcharris77@gmail.com>
2019-01-26 21:05:24 -0600

    allow player to choose between creating code and guessing filling out functions for game to work

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ruby_stuff
+Master mind is a game where 2 players take turns guessing a number of times an opponents code made with
+a number of different pieces where the code maker gives feedback with two different types of pieces about 
+which of the guessed code is correct. Those two feedback pieces represent: 1. that a guessed piece is in a
+correct spot in the right color, 2. and the correct piece played in the wrong position. If there are more of
+one type of guessed piece than there are of that type piece in the code then the extra pieces will not be
+awarded feedback.
+
+For example. Pieces 1..6 for a code 123154 and a guess of 443456 feedback could be 201010.
+
+add functionallity to allow player to choose whether he guesses or creates the code. game display prompt or player input prevents this from happenning. player needs ability to choose a code, this can be taken care of by player input.
+
+Computer generate code needs return replaced
--- mastermind.rb
+++ mastermind.rb
@@ -1,72 +1,25 @@
-=begin 
-Master mind is a game where 2 players take turns guessing a number of times an opponents code made with
-a number of different pieces where the code maker gives feedback with two different types of pieces about 
-which of the guessed code is correct. Those two feedback pieces represent: 1. that a guessed piece is in a
-correct spot in the right color, 2. and the correct piece played in the wrong position. If there are more of
-one type of guessed piece than there are of that type piece in the code then the extra pieces will not be
-awarded feedback.
-For example. Pieces 1..6 for a code 123154 and a guess of 443456 feedback could be 201010.
 
 There is a computer, player, computer code, guessed code, feedback code, game as a whole, counter for guesses,
 display, and prompt. As well as a way to reset and keep track of wins and losses.
-=end
 module Mastermind
     class Computer
-        attr_reader :code
         def generate_code
-            @code = ""
+            code = ""
             6.times do
-                @code += (1 + rand(6)).to_s
+                code += (1 + rand(6)).to_s
             end
-            @code = "121134"
+            return code
         end
-        def feedback(player_code)
-            feedback_code = "000000"
-            def check_for_correct(player_code, feedback_code)
-                (0..5).each do |x|
-                    if player_code[x] == @code[x]
-                        feedback_code[x] = "2"
-                    end
-                end
-            end
-            def check_for_wrong_spot(player_code, feedback_code)
-                #checks duplicates in case there are more than in code
-                duplicates = {}
-                (1..6).each do |num|
-                    (0..5).each do |index|
-                        if num.to_s == @code[index] && player_code[index] != @code[index]
-                            if duplicates[num] == nil
-                                duplicates[num] = 1
-                            elsif
-                                duplicates[num] += 1
-                            end
-                        end
-                    end
-                end
-                (0..5).each do |i|
-                    if player_code[i] != @code[i]
-                        duplicates.each do |x,y|
-                            if player_code[i].to_i == x && y > 0
-                                duplicates[x] = y - 1
-                                feedback_code[i] = "1"
-                            end
-                        end
-                    end
-                end
-            end
-            check_for_wrong_spot(player_code, feedback_code)
-            check_for_correct(player_code, feedback_code)
-            return feedback_code
-        end
+        def
     end
     class Player
-        def enter_input
-            guess = gets.chomp
+        def enter_code
+            code = gets.chomp
             unless guess =~ /^[1-6]{6}$/
                 puts "Enter a valid input"
-                guess = gets.chomp
+                code = gets.chomp
             end
-            return guess
+            return code
         end
     end
 
@@ -84,33 +37,100 @@ module Mastermind
             @computer_score = 0
             @player = Player.new
             @computer = Computer.new
-            @computer.generate_code
             puts "New game START"
+            self.new_game
         end
-        def display_prompt
-            puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
-            puts "Guesses left = #{@guesses_left}"
-            puts "Enter a six digit code"
-            puts "Feedback: 0 means no hit, 1 means right number wrong place, 2 means correct"
-            @choice = @player.enter_input
+        def display_prompt(@game_type)
+            if game_type == "guess"
+                puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
+                puts "Guesses left = #{@guesses_left}"
+                puts "Enter a six digit code"
+                puts "Feedback: 0 means no hit, 1 means right number wrong place, 2 means correct"
+                @choice = @player.enter_code
+            elsif game_type == 'code'
+                @choice = @computer.guess
+                puts "Computer guesses #{@choice}"
+            end
         end
-        def check_status
-            if @computer.feedback(@choice) == "222222"
-                @player_score += 1
+        def check_status(@game_type)
+            if self.feedback(@choice) == "222222"
+                if @game_type = "guess"
+                    @player_score += 1
+                else
+                    @computer_score += 1
+                    puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
+                end
                 @guesses_left = 12
-                @computer.generate_code
-                puts "You win! Starting new game"
-            elsif @computer.feedback(@choice) != "222222"
+                self.new_game
+                puts "Guesser wins! Starting new game"
+            elsif self.feedback(@choice) != "222222"
                 @guesses_left -= 1
                 if @guesses_left < 0
-                    @computer_score +=1
+                    if @game_type == "code"
+                        @computer_score += 1
+                    else
+                        @player_score += 1
+                        puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
+                    end
                     @guesses_left = 12
-                    @computer.generate_code
-                    puts "You lose! Starting new game"
+                    self.new_game
+                    puts "Coder loses! Starting new game"
                 else
-                    puts @computer.feedback(@choice)
+                    puts self.feedback(@choice)
                 end
             end
+        end
+        def new_game
+            puts "Enter guess to begin guessing role, code to create code for computer to guess."
+            @game_type = gets.chomp
+            unless game_type =~ /^guess$|^code$/
+                puts "Enter a valid input"
+                @game_type = gets.chomp
+            if @game_type = "guess"
+                @code = @computer.generate
+            else
+                puts "Enter your code!"
+                @code = @player.enter_code
+            end
+        end
+
+        def feedback(guess_code)
+            feedback_code = "000000"
+            def check_for_correct(guess_code, feedback_code)
+                (0..5).each do |x|
+                    if guess_code[x] == @code[x]
+                        feedback_code[x] = "2"
+                    end
+                end
+            end
+            def check_for_wrong_spot(guess_code, feedback_code)
+                #checks duplicates in case there are more than in code
+                duplicates = {}
+                (1..6).each do |num|
+                    (0..5).each do |index|
+                        if num.to_s == @code[index] && guess_code[index] != @code[index]
+                            if duplicates[num] == nil
+                                duplicates[num] = 1
+                            elsif
+                                duplicates[num] += 1
+                            end
+                        end
+                    end
+                end
+                (0..5).each do |i|
+                    if guess_code[i] != @code[i]
+                        duplicates.each do |x,y|
+                            if guess_code[i].to_i == x && y > 0
+                                duplicates[x] = y - 1
+                                feedback_code[i] = "1"
+                            end
+                        end
+                    end
+                end
+            end
+            check_for_wrong_spot(guess_code, feedback_code)
+            check_for_correct(guess_code, feedback_code)
+            return feedback_code
         end
         def game_loop
             while true
