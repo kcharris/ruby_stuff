03c3b10a0d2d87808ecb84477ca384eacdad73ad
kevin <kcharris77@gmail.com>
2019-02-02 21:06:57 -0600

    fix computer showing 13 guesses now shows 12

--- README.md
+++ README.md
@@ -8,6 +8,3 @@ awarded feedback.
 
 For example. Pieces 1..6 for a code 123154 and a guess of 443456 feedback could be 201010.
 
-add functionallity to allow player to choose whether he guesses or creates the code. game display prompt or player input prevents this from happenning. player needs ability to choose a code, this can be taken care of by player input.
-
-Computer generate code needs return replaced
--- mastermind.rb
+++ mastermind.rb
@@ -1,25 +1,27 @@
 
 module Mastermind
-	class Computer
-			def generate_code
-					code = ""
-					6.times do
-							code += (1 + rand(6)).to_s
-					end
-					return code
-			end
-	end
-	class Player
-			def enter_code
-					code = gets.chomp
-					until code =~ /^[1-6]{6}$/
-							puts "Enter a valid input"
-							code = gets.chomp
-					end
-					return code
-			end
-	end
-	class Game 
+  class Computer
+    def generate_code
+      code = ""
+      6.times do
+        code += (1 + rand(6)).to_s
+      end
+      return code
+    end
+  end
+
+  class Player
+    def enter_code
+      code = gets.chomp
+      until code =~ /^[1-6]{6}$/
+        puts "Enter a valid input"
+        code = gets.chomp
+      end
+      return code
+    end
+  end
+
+  class Game
     #display new game start
     #keep score between player and computer
     #Keep track of used codes
@@ -27,117 +29,127 @@ module Mastermind
     #reset game if win or player resign
     #display game
     def initialize
-        @guesses_left = 12
-        @player_score = 0
-        @computer_score = 0
-        @player = Player.new
-        @computer = Computer.new
-        puts "New game START"
-        self.new_game
+      @guesses_left = 12
+      @player_score = 0
+      @computer_score = 0
+      @player = Player.new
+      @computer = Computer.new
+      puts "New game START"
+      self.new_game
     end
+
     def display_prompt
-        if @game_type == "guess"
-            puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
-            puts "Guesses left = #{@guesses_left}"
-            puts "Enter a six digit code"
-            puts "Feedback: 0 means no hit, 1 means right number wrong place, 2 means correct"
-            @choice = @player.enter_code
-        elsif @game_type == 'code'
-            @choice = @computer.generate_code
-            puts "Computer guesses #{@choice}"
-        end
+      if @game_type == "guess"
+        puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
+        puts "Guesses left = #{@guesses_left}"
+        puts "Enter a six digit code"
+        puts "Feedback: 0 means no hit, 1 means right number wrong place, 2 means correct"
+        @choice = @player.enter_code
+      elsif @game_type == "code"
+        @choice = @computer.generate_code
+      end
     end
+
     def check_status
-        if self.feedback(@choice) == "222222"
-            if @game_type == "guess"
-                @player_score += 1
-                puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
-            else
-                @computer_score += 1
-                puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
-            end
-            @guesses_left = 12
-            self.new_game
-            puts "Guesser wins! Starting new game"
-        elsif self.feedback(@choice) != "222222"
-            @guesses_left -= 1
-            if @guesses_left < 0
-                if @game_type == "guess"
-                    @computer_score += 1
-                    puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
-                else
-                    @player_score += 1
-                    puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
-                end
-                @guesses_left = 12
-                self.new_game
-                puts "Coder loses! Starting new game"
-            else
-                puts self.feedback(@choice)
-            end
-        end
-    end
-    def new_game
-        puts "Enter guess to begin guessing role, code to create code for computer to guess."
-        @game_type = gets.chomp
-        until @game_type =~ /^guess$|^code$/
-            puts "Enter a valid input"
-            @game_type = gets.chomp
-        end
+      #
+      if self.feedback(@choice) == "222222"
         if @game_type == "guess"
-            @code = @computer.generate_code
+          @player_score += 1
+          puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
         else
-            puts "Enter your code!"
-            @code = @player.enter_code
+          @computer_score += 1
+          puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
         end
+        @guesses_left = 12
+        self.new_game
+        puts "Guesser wins! Starting new game"
+      elsif self.feedback(@choice) != "222222"
+        @guesses_left -= 1
+        if @guesses_left < 0
+          if @game_type == "guess"
+            @computer_score += 1
+            puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
+          else
+            @player_score += 1
+            puts "Score is Player: #{@player_score} vs Computer: #{@computer_score}"
+          end
+          @guesses_left = 12
+          self.new_game
+          puts "Coder loses! Starting new game"
+        elsif @game_type == "guess"
+          puts self.feedback(@choice)
+        else
+          puts "Computer guesses #{@choice}"
+          puts self.feedback(@choice)
+        end
+      end
+    end
+
+    def new_game
+      puts "Enter guess to begin guessing role, code to create code for computer to guess."
+      @game_type = gets.chomp
+      until @game_type =~ /^guess$|^code$/
+        puts "Enter a valid input"
+        @game_type = gets.chomp
+      end
+      if @game_type == "guess"
+        @code = @computer.generate_code
+      else
+        puts "Enter your code!"
+        @code = @player.enter_code
+      end
     end
 
     def feedback(guess_code)
-        feedback_code = "000000"
-        check_for_correct(guess_code, feedback_code)
-        check_for_wrong_spot(guess_code, feedback_code)
-        return feedback_code
+      feedback_code = "000000"
+      check_for_correct(guess_code, feedback_code)
+      check_for_wrong_spot(guess_code, feedback_code)
+      return feedback_code
     end
+
     def check_for_correct(guess_code, feedback_code)
-        (0..5).each do |x|
-            if guess_code[x] == @code[x]
-                feedback_code[x] = "2"
-            end
+      (0..5).each do |x|
+        if guess_code[x] == @code[x]
+          feedback_code[x] = "2"
         end
+      end
     end
+
     def check_for_wrong_spot(guess_code, feedback_code)
-        #checks duplicates in case there are more than in code
-        duplicates = {}
-        (1..6).each do |num|
-            (0..5).each do |index|
-                if num.to_s == @code[index] && guess_code[index] != @code[index]
-                    if duplicates[num] == nil
-                        duplicates[num] = 1
-                    else
-                        duplicates[num] += 1
-                    end
-                end
-            end
-        end
-        (0..5).each do |i|
-          if guess_code[i] != @code[i]
-            duplicates.each do |x,y|
-              if guess_code[i].to_i == x && y > 0
-                  duplicates[x] = y - 1
-                  feedback_code[i] = "1"
-              end
+      #checks duplicates in case there are more than in code
+      duplicates = {}
+      (1..6).each do |num|
+        (0..5).each do |index|
+          if num.to_s == @code[index] && guess_code[index] != @code[index]
+            if duplicates[num] == nil
+              duplicates[num] = 1
+            else
+              duplicates[num] += 1
             end
           end
-         end
+        end
       end
-			def game_loop
-					while true
-							self.display_prompt
-							self.check_status
-					end
-			end
+      (0..5).each do |i|
+        if guess_code[i] != @code[i]
+          duplicates.each do |x, y|
+            if guess_code[i].to_i == x && y > 0
+              duplicates[x] = y - 1
+              feedback_code[i] = "1"
+            end
+          end
+        end
+      end
+    end
+
+    def game_loop
+      while true
+        self.display_prompt
+        self.check_status
+      end
+    end
   end
 end
+
 include Mastermind
 game = Game.new
 game.game_loop
